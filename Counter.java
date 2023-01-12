fd0fc25e028c168515fdeff8927b247bf915098a
tekater <120014627+tekater@users.noreply.github.com>
2023-01-13 01:24:02 +0900

    Create Counter.java

---
+++ Counter.java
@@ -0,0 +1,15 @@
+public class Counter {
+    int current;
+    public Counter() {
+        this.current = current;
+    }
+    /*public void setCurrent(int current) {
+        this.current = current;
+    }*/
+    public int inc() {
+        return this.current++;
+    }
+    public int getCurrent() {
+        return this.current;
+    }
+}
